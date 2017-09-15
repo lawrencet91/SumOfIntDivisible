b224e20859a38ec441f453faacbe96c570ea1c21
Lawrence Tan <lawrence-916@hotmail.com>
2017-09-15 15:48:45 +1000

    Add project files.

---
+++ SumOfIntDividesibleBy10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfIntDivisible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Divide by: ");
+            string divideInput = Console.ReadLine();
+            char[] num = userInput.ToCharArray();
+            int total = 0;
+            foreach(char a in num)
+            {
+                total += Convert.ToInt32(a.ToString());
+            }
+
+            if (total%Convert.ToInt32(divideInput.ToString()) == 0)
+            {
+                Console.WriteLine(total + " Divisible by " + divideInput);
+            }else
+            {
+                Console.WriteLine(total + " Not divisible by " + divideInput);
+            }
+            Console.ReadLine();
+        }
+    }
+}
